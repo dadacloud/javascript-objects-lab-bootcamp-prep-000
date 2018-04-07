401bb7e19f4e4382afb7fc3f1596ad068d348471
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-07 19:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var obj = { prop: 1 };
-  Object.assign({prop2 : 2}, recipes)
+  return Object.assign({}, obj,{ [prop2] : 2});
+ // return Object.assign({}, obj, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
